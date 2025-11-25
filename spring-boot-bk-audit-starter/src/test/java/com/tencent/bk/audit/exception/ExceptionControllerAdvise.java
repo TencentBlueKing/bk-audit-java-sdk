bd17ae17707268552f7d78c21eb1c02dbc5de7bd
jsonwan <jsonwan@tencent.com>
2025-11-25 10:55:59 +0800

    perf: 升级依赖组件版本 #32
    1.升级Gradle、JDK、SpringBoot版本；
    2.升级snakeyaml等组件版本，消除安全告警。

--- spring-boot-bk-audit-starter/src/test/java/com/tencent/bk/audit/exception/ExceptionControllerAdvise.java
+++ spring-boot-bk-audit-starter/src/test/java/com/tencent/bk/audit/exception/ExceptionControllerAdvise.java
@@ -1,5 +1,6 @@
 package com.tencent.bk.audit.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ExceptionControllerAdvise extends ResponseEntityExceptionHandler {
